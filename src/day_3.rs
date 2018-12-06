c4eafd87025c8504584ba82b19637080d38b7810
Michael <michael@linux.fritz.box>
2018-12-06 22:22:00 +0100

    Tag 3 Stern 2

--- src/day_3.rs
+++ src/day_3.rs
@@ -2,6 +2,14 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::fs;
 
+struct Claim {
+    id: u32,
+    x: u32,
+    y: u32,
+    width: u32,
+    height: u32,
+}
+
 pub fn star_1() -> u32 {
     //1 ID; 2 X; 3 Y; 4 Breite; 5 Höhe
     let claim_re = Regex::new("#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)").unwrap();
@@ -33,4 +41,52 @@ pub fn star_1() -> u32 {
         }
     }
     double_claims
+}
+
+pub fn star_2() -> u32 {
+    //1 ID; 2 X; 3 Y; 4 Breite; 5 Höhe
+    let claim_re = Regex::new("#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)").unwrap();
+
+    let contents =
+        fs::read_to_string("./input/day_3.txt").expect("Something went wrong reading the file");
+
+    let mut fabric: HashMap<(u32, u32), u32> = HashMap::new();
+    let mut claim_list :Vec<Claim> = Vec::new();
+
+    for line in contents.lines() {
+        let claim_match = claim_re.captures(line).unwrap();
+
+        let claim = Claim {
+            id: claim_match[1].parse::<u32>().unwrap(),
+            x: claim_match[2].parse::<u32>().unwrap(),
+            y: claim_match[3].parse::<u32>().unwrap(),
+            width: claim_match[4].parse::<u32>().unwrap(),
+            height: claim_match[5].parse::<u32>().unwrap(),
+        };
+
+        for current_x in claim.x..claim.x + claim.width {
+            for current_y in claim.y..claim.y + claim.height {
+                *fabric.entry((current_x, current_y)).or_insert(0) += 1;
+            }
+        }
+        claim_list.push(claim);
+    }
+
+    let mut found = true;
+    for claim in claim_list.iter() {
+        for current_x in claim.x..claim.x + claim.width {
+            for current_y in claim.y..claim.y + claim.height {
+                if *fabric.get(&(current_x, current_y)).unwrap() != 1 {
+                    found = false;
+                }
+            }
+        }
+
+        if found == true {
+            return claim.id;
+        } else {
+            found = true;
+        }
+    }
+    0
 }
--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,5 @@ mod day_3;
 fn main() {
     println!("Day 1: {}, {}", day_1::star_1(), day_1::star_2());
     println!("Day 2: {}, {}", day_2::star_1(), day_2::star_2());
-    println!("Day 3: {}", day_3::star_1());
+    println!("Day 3: {}, {}", day_3::star_1(), day_3::star_2());
 }
