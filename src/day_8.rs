e2f9dc6fec3391a42efef33b1fa99ec0fc3882cd
Michael <michael@linux.fritz.box>
2018-12-08 16:24:31 +0100

    Tag 8 Stern 2

--- src/day_8.rs
+++ src/day_8.rs
@@ -10,10 +10,22 @@ pub fn star_1() -> u32 {
         .map(|x| x.trim().parse::<u32>().expect("Can not parse!"))
         .collect();
 
-    parse_element(&mut numbers)
+    parse_element(&mut numbers, &metadata_sum)
 }
 
-fn parse_element(element: &mut VecDeque<u32>) -> u32 {
+pub fn star_2() -> u32 {
+    let contents =
+        fs::read_to_string("./input/day_8.txt").expect("Something went wrong reading the file");
+
+    let mut numbers: VecDeque<u32> = contents
+        .split(" ")
+        .map(|x| x.trim().parse::<u32>().expect("Can not parse!"))
+        .collect();
+
+    parse_element(&mut numbers, &metadata_index)
+}
+
+fn parse_element(element: &mut VecDeque<u32>, f: &Fn(&mut VecDeque<u32>, u32, u32) -> u32) -> u32 {
     let child_count = if let Some(i) = element.pop_front() {
         i
     } else {
@@ -26,13 +38,13 @@ fn parse_element(element: &mut VecDeque<u32>) -> u32 {
         0
     };
 
-    metadata_sum(element, child_count, metadata_count)
+    f(element, child_count, metadata_count)
 }
 
 fn metadata_sum(element: &mut VecDeque<u32>, child_count: u32, metadata_count: u32) -> u32 {
     let mut meta_sum = 0;
     for _ in 0..child_count {
-        meta_sum += parse_element(element);
+        meta_sum += parse_element(element, &metadata_sum);
     }
 
     for _ in 0..metadata_count {
@@ -42,5 +54,44 @@ fn metadata_sum(element: &mut VecDeque<u32>, child_count: u32, metadata_count: u
             0
         };
     }
+    meta_sum
+}
+
+fn metadata_index(element: &mut VecDeque<u32>, child_count: u32, metadata_count: u32) -> u32 {
+    let mut node_values = Vec::new();
+
+    let mut meta_sum = 0;
+    if child_count > 0 {
+        for _ in 0..child_count {
+            node_values.push(parse_element(element, &metadata_index));
+        }
+
+        for _ in 0..metadata_count {
+            let index = if let Some(i) = element.pop_front() {
+                if i > 0 {
+                    (i - 1) as usize
+                } else {
+                    usize::max_value()
+                }
+            } else {
+                usize::max_value()
+            };
+
+            meta_sum += if let Some(&node_value) = node_values.get(index) {
+                node_value
+            } else {
+                0
+            }
+        }
+    } else {
+        for _ in 0..metadata_count {
+            meta_sum += if let Some(i) = element.pop_front() {
+                i
+            } else {
+                0
+            };
+        }
+    }
+
     meta_sum
 }
--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,5 @@ fn main() {
     //println!("Day 1: {}, {}", day_1::star_1(), day_1::star_2());
     //println!("Day 2: {}, {}", day_2::star_1(), day_2::star_2());
     //println!("Day 3: {}, {}", day_3::star_1(), day_3::star_2());
-    println!("Day 8: {}", day_8::star_1());
+    println!("Day 8: {}, {}", day_8::star_1(), day_8::star_2());
 }
