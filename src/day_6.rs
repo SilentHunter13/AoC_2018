6cefa4c25c19c052cb11ab35a39ebdc5c9307454
Michael <michael@linux.fritz.box>
2018-12-22 19:17:38 +0100

    Tag 6 Stern 1

---
+++ src/day_6.rs
@@ -0,0 +1,87 @@
+use std::fs;
+
+pub fn star_1() -> u32 {
+    let contents =
+        fs::read_to_string("./input/day_6.txt").expect("Something went wrong reading the file");
+
+    let mut places: Vec<(u32, u32)> = Vec::new();
+    let mut min_x = u32::max_value();
+    let mut min_y = u32::max_value();
+    let mut max_x = 0;
+    let mut max_y = 0;
+
+    //input parsen
+    for line in contents.lines() {
+        let mut coordinates = line
+            .split(',')
+            .map(|x| x.trim().parse::<u32>().expect("Parse Error!"));
+
+        let x = coordinates.next().expect("no x");
+        let y = coordinates.next().expect("no y");
+
+        min_x = x.min(min_x);
+        min_y = x.min(min_y);
+        max_x = x.max(max_x);
+        max_y = x.max(max_y);
+
+        places.push((x, y));
+    }
+
+    #[derive(Clone, Debug)]
+    struct Place {
+        size: u32,
+        infinite: bool,
+    };
+
+    let mut results = vec![
+        Place {
+            size: 0,
+            infinite: false
+        };
+        places.len()
+    ];
+
+    //für jeden Punkt den Besitzer bestimmen
+    for x in min_x..=max_x {
+        for y in min_y..=max_y {
+            let owner = get_owner(&places, x, y);
+            if let Ok(index) = owner {
+                results[index].size += 1;
+            }
+
+            //Wenn der Punkt auf dem Rand liegt ist die Fläche unbegrenzt
+            if x == min_x || x == max_x || y == min_y || y == max_y {
+                if let Ok(index) = owner {
+                    results[index].infinite = true;
+                }
+            }
+        }
+    }
+
+    //unbegrenzte Flächen entfernen
+    results.retain(|x| !x.infinite);
+    //größte Fläche finden
+    let result = results.iter().max_by_key(|x| x.size);
+
+    if let Some(element) = result {
+        element.size
+    } else {
+        0
+    }
+}
+
+fn get_owner(places: &Vec<(u32, u32)>, x: u32, y: u32) -> Result<usize, ()> {
+    let mut min_distance = u32::max_value();
+    let mut ret_val = Err(());
+    for (i, place) in places.iter().enumerate() {
+        let distance = x.max(place.0) - x.min(place.0) + y.max(place.1) - y.min(place.1);
+
+        if distance < min_distance {
+            min_distance = distance;
+            ret_val = Ok(i);
+        } else if distance == min_distance {
+            ret_val = Err(());
+        }
+    }
+    ret_val
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod day_22;
 mod day_3;
 mod day_4;
 mod day_5;
+mod day_6;
 mod day_7;
 mod day_8;
 
@@ -16,6 +17,7 @@ fn main() {
     println!("Day 3: {}, {}", day_3::star_1(), day_3::star_2());
     println!("Day 4: {}, {}", day_4::star_1(), day_4::star_2());
     println!("Day 5: {}, {}", day_5::star_1(), day_5::star_2());
+    println!("Day 6: {}", day_6::star_1());
     println!("Day 7: {}, {}", day_7::star_1(), day_7::star_2());
     println!("Day 8: {}, {}", day_8::star_1(), day_8::star_2());
     day_10::star_1();
