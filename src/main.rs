ca63b8cf2a742b81e60cfb326f0244a9374d6c27
Michael <michael@linux.fritz.box>
2018-12-22 13:26:52 +0100

    Tag 5 Stern 2

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     println!("Day 2: {}, {}", day_2::star_1(), day_2::star_2());
     println!("Day 3: {}, {}", day_3::star_1(), day_3::star_2());
     println!("Day 4: {}, {}", day_4::star_1(), day_4::star_2());
-    println!("Day 5: {}", day_5::star_1());
+    println!("Day 5: {}, {}", day_5::star_1(), day_5::star_2());
     println!("Day 7: {}, {}", day_7::star_1(), day_7::star_2());
     println!("Day 8: {}, {}", day_8::star_1(), day_8::star_2());
     day_10::star_1();
