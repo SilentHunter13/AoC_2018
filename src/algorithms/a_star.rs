f65376820c4f45d1ca6b5629187252b0e024be09
Michael <michael@localhost.localdomain>
2019-02-10 17:16:56 +0100

    A Stern Algorithmus über Traits nutzbar gemacht.

---
+++ src/algorithms/a_star.rs
@@ -0,0 +1,138 @@
+use std::collections::HashMap;
+
+pub trait Cost<P> {
+    fn cost(&self, position: &P) -> u32;
+}
+
+pub trait Neighbour<P> {
+    fn neighbours_iter(&self, pos: P) -> Vec<P>;
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Attributes<P> {
+    g: u32,                 //minimale Kosten, um zu dieser Positio zu kommen
+    predecessor: Option<P>, //Vorgünger Knoten
+}
+
+pub struct AStar<G, P> {
+    open_list: HashMap<P, (u32, Attributes<P>)>, // Prioritätenwarteschlange
+    closed_list: HashMap<P, Attributes<P>>,
+    map: G,
+    next: Option<P>,
+}
+
+impl<G, P> AStar<G, P>
+where
+    P: std::cmp::Eq + std::hash::Hash + Copy + Cost<P>,
+    G: Neighbour<P>,
+{
+    pub fn new(map: G) -> AStar<G, P>
+    where
+        P: std::cmp::Eq + std::hash::Hash,
+    {
+        AStar {
+            open_list: HashMap::new(),
+            closed_list: HashMap::new(),
+            map,
+            next: None,
+        }
+    }
+    pub fn calc_path(&mut self, start: P, target: P) -> Option<u32> {
+        // Initialisierung der Open List, die Closed List ist noch leer
+        // (die Priorität bzw. der f-Wert des Startknotens ist unerheblich)
+        self.open_list.insert(
+            start,
+            (
+                0,
+                Attributes {
+                    g: 0,
+                    predecessor: None,
+                },
+            ),
+        );
+        // diese Schleife wird durchlaufen bis entweder
+        // - die optimale Lösung gefunden wurde oder
+        // - feststeht, dass keine Lösung existiert
+        loop {
+            // Knoten mit dem geringsten f-Wert aus der Open List entfernen
+            if let Some((current_node, _)) = self.open_list.iter().min_by_key(|x| ((*x).1).0) {
+                let copied_current_node = current_node.clone();
+
+                let (removed_current_node, (_, attr)) = self
+                    .open_list
+                    .remove_entry(&copied_current_node)
+                    .expect("node must be in");
+
+                // Der aktuelle Knoten soll durch nachfolgende Funktionen
+                // nicht weiter untersucht werden, damit keine Zyklen entstehen
+                self.closed_list.insert(removed_current_node, attr);
+
+                // Wurde das Ziel gefunden?
+                if removed_current_node == target {
+                    self.next = Some(removed_current_node);
+                    break Some(attr.g); //PathFound;
+                }
+
+                // Wenn das Ziel noch nicht gefunden wurde: Nachfolgeknoten
+                // des aktuellen Knotens auf die Open List setzen
+                self.expand_node(removed_current_node, target, attr);
+            } else {
+                // die Open List ist leer, es existiert kein Pfad zum Ziel
+                break None;
+            }
+        }
+    }
+    fn expand_node(&mut self, node: P, target: P, attr: Attributes<P>) {
+        for successor in self.map.neighbours_iter(node) {
+            // wenn der Nachfolgeknoten bereits auf der Closed List ist – tue nichts
+            if !self.closed_list.contains_key(&successor) {
+                // g-Wert für den neuen Weg berechnen: g-Wert des Vorgängers plus
+                // die Kosten der gerade benutzten Kante
+                let tentative_g = attr.g + successor.cost(&node);
+                let f = tentative_g + successor.cost(&target); //Berechnung der Kosten und Abschätzun zum Ziel hier gleich
+
+                //wenn der Nachfolgeknoten schon in der open_list ist
+                if let Some((f_s, attributes)) = self.open_list.get_mut(&successor) {
+                    //wenn der neue Weg besser als der alte ist
+                    if attributes.g > tentative_g {
+                        attributes.predecessor = Some(node);
+                        attributes.g = tentative_g;
+                        *f_s = f;
+                    }
+                } else {
+                    //Nachfolgeknoten ist noch nicht in der open_list
+                    self.open_list.insert(
+                        successor,
+                        (
+                            f,
+                            Attributes {
+                                g: tentative_g,
+                                predecessor: Some(node),
+                            },
+                        ),
+                    );
+                }
+            }
+        }
+    }
+}
+
+impl<G, P> Iterator for AStar<G, P>
+where
+    P: std::cmp::Eq + std::hash::Hash + Copy,
+{
+    type Item = P;
+
+    fn next(&mut self) -> Option<P> {
+        let ret_val;
+        match self.next {
+            None => None,
+            Some(next) => {
+                ret_val = self.next;
+                let next_item = self.closed_list.get(&next).expect("Item not available");
+                self.next = next_item.predecessor;
+                ret_val
+            }
+        }
+    }
+}
--- src/day_22.rs
+++ src/day_22.rs
@@ -1,3 +1,5 @@
+use crate::algorithms::a_star;
+use crate::algorithms::a_star::AStar;
 use std::collections::HashMap;
 use std::fmt;
 
@@ -5,11 +7,6 @@ use std::fmt;
 const DEPTH: u32 = 11739;
 const TARGET_X: u32 = 11;
 const TARGET_Y: u32 = 718;
-
-//Test Puzzle Input
-// const DEPTH: u32 = 510;
-// const TARGET_X: u32 = 10;
-// const TARGET_Y: u32 = 10;
 
 //Game Constants
 const X_FACTOR: u32 = 16807;
@@ -75,7 +72,7 @@ struct Position {
     gear: Gear,
 }
 
-impl Position {
+impl a_star::Cost<Position> for Position {
     fn cost(&self, position: &Position) -> u32 {
         let mut cost = 0;
 
@@ -86,125 +83,6 @@ impl Position {
         cost += (position.coordinates.0 as i32 - self.coordinates.0 as i32).abs() as u32;
         cost += (position.coordinates.1 as i32 - self.coordinates.1 as i32).abs() as u32;
         cost
-    }
-}
-
-#[derive(Clone, Copy, Debug)]
-struct Attributes {
-    g: u32,                        //minimale Kosten, um zu dieser Positio zu kommen
-    predecessor: Option<Position>, //Vorgünger Knoten
-}
-
-struct AStar {
-    open_list: HashMap<Position, (u32, Attributes)>, // Prioritätenwarteschlange
-    closed_list: HashMap<Position, Attributes>,
-    map: Region,
-    next: Option<Position>,
-}
-
-impl AStar {
-    fn new(map: Region) -> AStar {
-        AStar {
-            open_list: HashMap::new(),
-            closed_list: HashMap::new(),
-            map,
-            next: None,
-        }
-    }
-    fn calc_path(&mut self, start: Position, target: Position) -> Option<u32> {
-        // Initialisierung der Open List, die Closed List ist noch leer
-        // (die Priorität bzw. der f-Wert des Startknotens ist unerheblich)
-        self.open_list.insert(
-            start,
-            (
-                0,
-                Attributes {
-                    g: 0,
-                    predecessor: None,
-                },
-            ),
-        );
-        // diese Schleife wird durchlaufen bis entweder
-        // - die optimale Lösung gefunden wurde oder
-        // - feststeht, dass keine Lösung existiert
-        loop {
-            // Knoten mit dem geringsten f-Wert aus der Open List entfernen
-            if let Some((current_node, _)) = self.open_list.iter().min_by_key(|x| ((*x).1).0) {
-                let copied_current_node = current_node.clone();
-
-                let (removed_current_node, (_, attr)) = self
-                    .open_list
-                    .remove_entry(&copied_current_node)
-                    .expect("node must be in");
-
-                // Der aktuelle Knoten soll durch nachfolgende Funktionen
-                // nicht weiter untersucht werden, damit keine Zyklen entstehen
-                self.closed_list.insert(removed_current_node, attr);
-
-                // Wurde das Ziel gefunden?
-                if removed_current_node == target {
-                    self.next = Some(removed_current_node);
-                    break Some(attr.g); //PathFound;
-                }
-
-                // Wenn das Ziel noch nicht gefunden wurde: Nachfolgeknoten
-                // des aktuellen Knotens auf die Open List setzen
-                self.expand_node(removed_current_node, target, attr);
-            } else {
-                // die Open List ist leer, es existiert kein Pfad zum Ziel
-                break None;
-            }
-        }
-    }
-    fn expand_node(&mut self, node: Position, target: Position, attr: Attributes) {
-        for successor in self.map.neighbours_iter(node) {
-            // wenn der Nachfolgeknoten bereits auf der Closed List ist – tue nichts
-            if !self.closed_list.contains_key(&successor) {
-                // g-Wert für den neuen Weg berechnen: g-Wert des Vorgängers plus
-                // die Kosten der gerade benutzten Kante
-                let tentative_g = attr.g + successor.cost(&node);
-                let f = tentative_g + successor.cost(&target); //Berechnung der Kosten und Abschätzun zum Ziel hier gleich
-
-                //wenn der Nachfolgeknoten schon in der open_list ist
-                if let Some((f_s, attributes)) = self.open_list.get_mut(&successor) {
-                    //wenn der neue Weg besser als der alte ist
-                    if attributes.g > tentative_g {
-                        attributes.predecessor = Some(node);
-                        attributes.g = tentative_g;
-                        *f_s = f;
-                    }
-                } else {
-                    //Nachfolgeknoten ist noch nicht in der open_list
-                    self.open_list.insert(
-                        successor,
-                        (
-                            f,
-                            Attributes {
-                                g: tentative_g,
-                                predecessor: Some(node),
-                            },
-                        ),
-                    );
-                }
-            }
-        }
-    }
-}
-
-impl Iterator for AStar {
-    type Item = Position;
-
-    fn next(&mut self) -> Option<Position> {
-        let ret_val;
-        match self.next {
-            None => None,
-            Some(next) => {
-                ret_val = self.next;
-                let next_item = self.closed_list.get(&next).expect("Item not available");
-                self.next = next_item.predecessor;
-                ret_val
-            }
-        }
     }
 }
 
@@ -266,6 +144,9 @@ impl Region {
             _ => panic!("modulo 3 above 2"),
         }
     }
+}
+
+impl a_star::Neighbour<Position> for Region {
     fn neighbours_iter(&self, pos: Position) -> Vec<Position> {
         let mut list = Vec::new();
 
