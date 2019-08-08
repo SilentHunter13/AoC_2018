b0b5271dcb6c8dc116d8803f17e5d57203872786
SilentHunter13 <34520758+SilentHunter13@users.noreply.github.com>
2019-08-08 22:35:40 +0200

    Clippy Hinweise behoben

--- src/day_8.rs
+++ src/day_8.rs
@@ -6,7 +6,7 @@ pub fn star_1() -> u32 {
         fs::read_to_string("./input/day_8.txt").expect("Something went wrong reading the file");
 
     let mut numbers: VecDeque<u32> = contents
-        .split(" ")
+        .split(' ')
         .map(|x| x.trim().parse::<u32>().expect("Can not parse!"))
         .collect();
 
@@ -18,14 +18,17 @@ pub fn star_2() -> u32 {
         fs::read_to_string("./input/day_8.txt").expect("Something went wrong reading the file");
 
     let mut numbers: VecDeque<u32> = contents
-        .split(" ")
+        .split(' ')
         .map(|x| x.trim().parse::<u32>().expect("Can not parse!"))
         .collect();
 
     parse_element(&mut numbers, &metadata_index)
 }
 
-fn parse_element(element: &mut VecDeque<u32>, f: &Fn(&mut VecDeque<u32>, u32, u32) -> u32) -> u32 {
+fn parse_element(
+    element: &mut VecDeque<u32>,
+    f: &dyn Fn(&mut VecDeque<u32>, u32, u32) -> u32,
+) -> u32 {
     let child_count = if let Some(i) = element.pop_front() {
         i
     } else {
