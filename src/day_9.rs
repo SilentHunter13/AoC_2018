e166f447de60204fce689b110a5259de4ffaacce
SilentHunter13 <34520758+SilentHunter13@users.noreply.github.com>
2019-10-06 18:41:45 +0200

    Funktion zum Spielen erstellt

--- src/day_9.rs
+++ src/day_9.rs
@@ -4,21 +4,29 @@ const PLAYER_COUNT: usize = 419;
 //const PLAYER_COUNT: usize = 9;
 
 pub fn star_1() -> usize {
+    play_marble_game(PLAYER_COUNT, MARBLE_MAX)
+}
+
+pub fn star_2() -> usize {
+    play_marble_game(PLAYER_COUNT, MARBLE_MAX * 100)
+}
+
+fn play_marble_game(players: usize, marbles: usize) -> usize {
     let mut board: Vec<usize> = vec![0];
-    let mut players: Vec<usize> = vec![0; PLAYER_COUNT];
+    let mut players: Vec<usize> = vec![0; players];
     let mut current_marble = 0;
 
-    for i in 1..=MARBLE_MAX {
+    for i in 1..=marbles {
         //aktuelle Position berechnen
         if (i % 23) == 0 {
             players[i % PLAYER_COUNT] += i;
 
-            let remove_index = (current_marble + board.len() - 7) % board.len();
-            players[i % PLAYER_COUNT] += board.remove(remove_index);
+            current_marble = (current_marble + board.len() - 7) % board.len();
 
-            current_marble = remove_index;
+            //Murmel entfernen
+            players[i % PLAYER_COUNT] += board.remove(current_marble);
         } else {
-            current_marble = (current_marble + 1) % (board.len()) + 1;
+            current_marble = (current_marble + 2) % board.len();
 
             //Murmel einfügen
             board.insert(current_marble, i);
