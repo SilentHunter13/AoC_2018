1933e37e7f59ffbd947678c1e5175ef5b2c76a6b
SilentHunter13 <34520758+SilentHunter13@users.noreply.github.com>
2019-10-31 16:22:38 +0100

    Tag 20 zu langsam

--- src/days/day_20.rs
+++ src/days/day_20.rs
@@ -1,127 +1,171 @@
 use std::collections::HashMap;
 use std::fs;
-use std::iter::FromIterator;
 
-#[derive(Debug)]
-struct State {
-    position: (i32, i32),
-    alternatives: Vec<usize>,
-    brace_depth: u32,
+#[derive(Default)]
+struct Room {
+    doors: [bool; 4],
+    distance: usize,
 }
 
-pub fn star_1() {
+#[derive(Debug)]
+struct Instruction {
+    instructions: String,
+    successors: Vec<usize>,
+}
+
+impl Instruction {
+    pub fn new() -> Instruction {
+        Instruction {
+            instructions: String::new(),
+            successors: Vec::new(),
+        }
+    }
+}
+
+pub fn star_1() -> usize {
+    let instructions = parse_instructions();
+
+    let mut map = HashMap::new();
+
+    go(&instructions, 0, (0, 0), &mut map);
+
+    //show_map(&map, false);
+
+    find_largest_distance(&map)
+}
+
+struct State {
+    alternatives_start: Vec<usize>,
+    alternatives_end: Vec<usize>,
+    predecessor: usize,
+}
+
+fn parse_instructions() -> Vec<Instruction> {
     let contents =
         fs::read_to_string("./input/day_20.txt").expect("Something went wrong reading the file");
 
-    let directions = Vec::from_iter(contents.chars());
+    let mut instructions: Vec<Instruction> = vec![Instruction::new()];
 
-    let mut stack: Vec<State> = Vec::new();
-    let mut index = 0;
-    let mut position = (0, 0);
-    let mut map: HashMap<(i32, i32), [bool; 4]> = HashMap::new();
-    let mut brace_depth = 0;
-    let mut wait_for_closing_brace = false;
-    let mut ignore_until = 0;
+    let mut state_stack = Vec::new();
 
-    loop {
-        let direction = directions[index];
+    let mut state = State {
+        alternatives_start: vec![],
+        alternatives_end: vec![],
+        predecessor: 0,
+    };
 
-        match direction {
+    let mut current_index = 0;
+
+    for char in contents.chars() {
+        match char {
             '(' => {
-                brace_depth += 1;
+                state_stack.push(state);
+                state = State {
+                    predecessor: current_index,
+                    alternatives_start: vec![instructions.len()],
+                    alternatives_end: Vec::new(),
+                };
+                current_index = instructions.len();
+
+                instructions.push(Instruction::new());
+            }
+            '|' => {
+                state.alternatives_end.push(current_index);
+                current_index = instructions.len();
+                state.alternatives_start.push(current_index);
+
+                instructions.push(Instruction::new());
             }
             ')' => {
-                if brace_depth == ignore_until {
-                    wait_for_closing_brace = false;
-                }
-                brace_depth -= 1;
-            }
-            _ => (),
-        }
+                state.alternatives_end.push(current_index);
+                current_index = instructions.len();
 
-        if !wait_for_closing_brace {
-            match direction {
-                '$' => {
-                    let mut found = false;
-                    let mut ready = false;
+                instructions.push(Instruction::new());
+                for alt in state.alternatives_start {
+                    instructions[state.predecessor].successors.push(alt);
+                }
+                for alt in state.alternatives_end {
+                    instructions[alt].successors.push(current_index);
+                }
 
-                    while !ready {
-                        if let Some(mut state) = stack.pop() {
-                            if let Some(alternative) = state.alternatives.pop() {
-                                index = alternative;
-                                position = state.position;
-                                brace_depth = state.brace_depth;
-                                stack.push(state);
-                                found = true;
-                                ready = true;
-                            }
-                        } else {
-                            ready = true;
-                        }
-                    }
-                    if !found {
-                        break;
-                    }
-                }
-                '(' => {
-                    stack.push(State {
-                        position,
-                        alternatives: Vec::new(),
-                        brace_depth,
-                    });
-                }
-                '|' => {
-                    let state_index = stack
-                        .iter()
-                        .position(|x| x.brace_depth == brace_depth)
-                        .expect("Es muss eine Klammer offen sein!");
-                    let last_state = &mut stack[state_index];
-                    if !last_state.alternatives.contains(&index) {
-                        last_state.alternatives.push(index);
-                    }
-                    wait_for_closing_brace = true;
-                    ignore_until = brace_depth;
-                }
-                'N' => {
-                    let old_room = map.entry(position).or_default();
-                    old_room[0] = true;
-                    position.1 += 1;
-                    let new_room = map.entry(position).or_default();
-                    new_room[2] = true;
-                }
-                'E' => {
-                    let old_room = map.entry(position).or_default();
-                    old_room[1] = true;
-                    position.0 += 1;
-                    let new_room = map.entry(position).or_default();
-                    new_room[3] = true;
-                }
-                'S' => {
-                    let old_room = map.entry(position).or_default();
-                    old_room[2] = true;
-                    position.1 -= 1;
-                    let new_room = map.entry(position).or_default();
-                    new_room[0] = true;
-                }
-                'W' => {
-                    let old_room = map.entry(position).or_default();
-                    old_room[3] = true;
-                    position.0 -= 1;
-                    let new_room = map.entry(position).or_default();
-                    new_room[1] = true;
-                }
-                _ => {} //alles andere ignorieren
+                state = state_stack.pop().expect("Es ist keine Klammer offen!");
             }
+            'N' | 'E' | 'S' | 'W' => {
+                instructions[current_index].instructions.push(char);
+            }
+            _ => {}
         }
-        index += 1;
     }
-
-    show_map(&map);
+    //println!("{:?}", instructions);
+    instructions
 }
 
-//fn load_map()
+fn go(
+    instructions: &Vec<Instruction>,
+    start_index: usize,
+    start_position: (i32, i32),
+    map: &mut HashMap<(i32, i32), Room>,
+) {
+    let position = go_steps(start_position, &instructions[start_index].instructions, map);
 
-fn show_map(map: &HashMap<(i32, i32), [bool; 4]>) {
+    for successor in &instructions[start_index].successors {
+        go(&instructions, *successor, position, map);
+    }
+}
+
+fn go_steps(
+    start: (i32, i32),
+    instructions: &String,
+    map: &mut HashMap<(i32, i32), Room>,
+) -> (i32, i32) {
+    let mut position = start;
+    for inst in instructions.chars() {
+        let old_room = map.entry(position).or_default();
+        let old_distance = old_room.distance;
+        match inst {
+            'N' => {
+                old_room.doors[0] = true;
+                position.1 += 1;
+                let new_room = map.entry(position).or_default();
+                new_room.doors[2] = true;
+                new_room.distance = old_distance + 1;
+            }
+            'E' => {
+                old_room.doors[1] = true;
+                position.0 += 1;
+                let new_room = map.entry(position).or_default();
+                new_room.doors[3] = true;
+                new_room.distance = old_distance + 1;
+            }
+            'S' => {
+                old_room.doors[2] = true;
+                position.1 -= 1;
+                let new_room = map.entry(position).or_default();
+                new_room.doors[0] = true;
+                new_room.distance = old_distance + 1;
+            }
+            'W' => {
+                old_room.doors[3] = true;
+                position.0 -= 1;
+                let new_room = map.entry(position).or_default();
+                new_room.doors[1] = true;
+                new_room.distance = old_distance + 1;
+            }
+            _ => {} //alles andere ignorieren
+        }
+    }
+    position
+}
+
+fn find_largest_distance(map: &HashMap<(i32, i32), Room>) -> usize {
+    map.iter()
+        .max_by_key(|(_, x)| x.distance)
+        .expect("Es gibt kein Maximum!")
+        .1
+        .distance
+}
+
+fn show_map(map: &HashMap<(i32, i32), Room>, show_distance: bool) {
     let min_x = (map.iter().min_by_key(|x| (x.0).0).expect("gibts").0).0;
     let min_y = (map.iter().min_by_key(|x| (x.0).1).expect("gibts").0).1;
     let max_x = (map.iter().max_by_key(|x| (x.0).0).expect("gibts").0).0;
@@ -136,15 +180,22 @@ fn show_map(map: &HashMap<(i32, i32), [bool; 4]>) {
                 if x == 0 && y == 0 {
                     line1.push('X');
                 } else {
-                    line1.push('.');
+                    if show_distance {
+                        line1.push(
+                            std::char::from_digit((room.distance % 10) as u32, 10)
+                                .expect("hat nur eine Stelle"),
+                        );
+                    } else {
+                        line1.push('.');
+                    }
                 }
-                if room[1] {
+                if room.doors[1] {
                     line1.push('|');
                 } else {
                     line1.push('#');
                 }
 
-                if room[2] {
+                if room.doors[2] {
                     line2.push('-');
                 } else {
                     line2.push('#');
